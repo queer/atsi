eca2530fbff94bffc62c5d5447093967ff0cc502
amy null <null@amy.gg>
2022-06-10 09:56:33 -0400

    📝 doc: Update help and README

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # @
 
-> the fastest immutable alpine container in the west
+> instant alpine shells
 
-Run [`cargo make debug`](https://github.com/sagiegurari/cargo-make) to get started.
+@ (atsi, "at-sign") is a tool for instantly provisioning interactive Alpine
+containers. Think of it a little like
+[`nix-shell`](https://nixos.org/manual/nix/stable/command-ref/nix-shell.html)
+for Alpine.
 
-Or `cargo make release` to get really started.
+## Getting started
+
+Run [`cargo make debug`](https://github.com/sagiegurari/cargo-make) to create
+a debug build, or run `cargo make release` to create a release build. Builds
+are emitted to `target/{debug,release}/@`.
+
+You may need to run `cargo install cargo-make` first.
--- src/main.rs
+++ src/main.rs
@@ -30,38 +30,61 @@ async fn main() -> SyncResult<()> {
         .subcommand(
             Command::new("run")
                 .visible_alias("r")
-                .arg(Arg::new("command").takes_value(true).required(true))
+                .arg(
+                    Arg::new("command")
+                        .takes_value(true)
+                        .help("The command to run inside of the container. Defaults to `sh`.")
+                        .default_value("sh")
+                        .long_help("The command to run inside of the container. This command will be pid 1\
+                            inside the container, and will have a bare-minimum environment set up.\n\
+                            \n\
+                            The default value for the command is `sh`, to just always get a shell.\n\
+                            \n\
+                            Examples:\n\
+                            - Get a shell: `@ run`\n\
+                            - Install Python 3: `@ run -P python3`")
+                )
                 // .arg(Arg::new("detach").short('d').required(false))
                 .arg(
                     Arg::new("immutable")
                         .short('i')
                         .long("immutable")
                         .required(false)
-                        .takes_value(false),
+                        .takes_value(false)
+                        .help("Makes the container's rootfs immutable (read-only).")
+                        ,
                 )
                 .arg(
                     Arg::new("package")
                         .short('P')
                         .multiple_occurrences(true)
-                        .takes_value(true),
+                        .takes_value(true)
+                        .help("Specify a package to install. Can be specified multiple times.")
+                        ,
                 )
                 .arg(
                     Arg::new("port")
                         .short('p')
                         .multiple_occurrences(true)
-                        .takes_value(true),
+                        .takes_value(true)
+                        .help("Expose a port to the host. Format is outer:inner, ex. `8080:8081`.")
+                        ,
                 )
                 .arg(
                     Arg::new("rw")
                         .long("rw")
                         .multiple_occurrences(true)
-                        .takes_value(true),
+                        .takes_value(true)
+                        .help("Mount a file/directory read-write. Format is source:target, ex. `/home/me/file:/file`.")
+                        ,
                 )
                 .arg(
                     Arg::new("ro")
                         .long("ro")
                         .multiple_occurrences(true)
-                        .takes_value(true),
+                        .takes_value(true)
+                        .help("Mount a file/directory read-only. Format is source:target, ex. `/home/me/file:/file`.")
+                        ,
                 ),
         )
         .subcommand(Command::new("ps"))
@@ -89,20 +112,14 @@ async fn main() -> SyncResult<()> {
             let rw_mounts: Vec<(String, String)> = matches.values_of("rw").map_or(vec![], |v| {
                 v.map(|p| {
                     let slice: Vec<String> = p.split(':').map(|s| s.to_string()).collect();
-                    (
-                        slice[0].clone(),
-                        slice[1].clone(),
-                    )
+                    (slice[0].clone(), slice[1].clone())
                 })
                 .collect()
             });
             let ro_mounts: Vec<(String, String)> = matches.values_of("ro").map_or(vec![], |v| {
                 v.map(|p| {
                     let slice: Vec<String> = p.split(':').map(|s| s.to_string()).collect();
-                    (
-                        slice[0].clone(),
-                        slice[1].clone(),
-                    )
+                    (slice[0].clone(), slice[1].clone())
                 })
                 .collect()
             });
